d331184b5d30ee537faa0af07e29d7aa6612f770
fmeraut <francois.meraut@hotmail.fr>
2021-08-24 16:32:52 +0200

    first commit

---
+++ src/main/java/com/BestTripApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTripApplication.class, args);
+	}
+
+}
